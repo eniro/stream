3032699fd877fe00dda14119e96affd5db80c8b1
Iro Enezli <enezliro@gmail.com>
2018-11-28 16:32:13 +0200

    added buttons

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -10,18 +10,33 @@ class GoogleAuth extends React.Component {
                 scope: 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
     }
 
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    };
+
     renderAuthButton() {
         if(this.state.isSignedIn === null) {
-            return <div>I dont know if we are signed in</div>;
+            return null;
         } else if (this.state.isSignedIn) {
-            return <div>I am signed In!</div>
+            return (
+                <button className="ui red google button">
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            );
         } else {
-            return <div>I am not signed in</div>;
+            return (
+                <button className="ui red google button">
+                    <i className="google icon" />
+                    Sing In with Google
+                </button>
+            );
         }
     }
     
