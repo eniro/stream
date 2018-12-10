40db49a25d3425689863059b41ddcfe74118070b
Iro Enezli <enezliro@gmail.com>
2018-12-10 09:51:14 +0200

    onDismiss from the parent

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -5,10 +5,13 @@ import history from '../history';
 const Modal = props => {
     return ReactDOM.createPortal(
         <div
-         onClick={() => history.push ('/')} 
+         onClick={props.onDismiss} 
          className='ui dimmer modals visible active'
         >
-            <div onClick={(e) => e.stopPropagation()} className="ui standard modal visible active">
+            <div 
+              onClick={(e) => e.stopPropagation()} 
+              className="ui standard modal visible active"
+            >
                 <div className="header">{props.title}</div>
                 <div className="content">
                     {props.content}
